188b5cb30c9b4171821f5736a8454f3fccf16c88
Johnny Jefferson <johnnyjeffersonjr@hotmail.com>
2020-12-02 22:22:12 -0300

    incluindo as pastas necessarias para o projeto

---
+++ modulo-01-javascript/desafio-01/js/main.js
@@ -0,0 +1,13 @@
+const home = document.querySelector('.home')
+
+function quertorSseo => {
+  (if of home) {
+    console.log(`Será apresentado o valor referente ${quertorSseo}, fuction`);
+  }
+}
+
+document.queryCommandEnabled(gof){
+  Boolean.of(classe) {
+    console.log(`Sorry`);
+  }
+}
